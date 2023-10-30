40cba69717f4283afa4d25bbf5a2d3ad3d7993ad
Keranod <konrad.konkel@wp.pl>
2023-10-30 13:22:06 +0000

    Handling login

--- src/App.jsx
+++ src/App.jsx
@@ -138,10 +138,14 @@ const App = () => {
       <h1>Notes</h1>
       <Notification message={errorMessage} />
 
-      {user === null ?
-      loginForm() :
-      noteForm()
+      {!user && loginForm()} 
+    {user && <div>
+       <p>{user.name} logged in</p>
+         {noteForm()}
+      </div>
     }
+
+    <h2>Notes</h2>
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
